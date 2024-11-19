f1d113651c5541a5043d90630c4816233e2098b1
Lennert Van Geert <lennert.vangeert@gmail.com>
2024-11-19 15:54:18 +0100

    scoreboard

--- app/src/pages/ScoreBoard/Scoreboard.jsx
+++ app/src/pages/ScoreBoard/Scoreboard.jsx
@@ -1,14 +1,30 @@
 import React, { useEffect, useState } from "react";
 import styles from "./Scoreboard.module.css"; // Import CSS module
 import { useChallenges } from "../../contexts/ChallengeProvider";
+import { useAuthContext } from "../../contexts/AuthContainer";
 
 const ScoreBoard = () => {
   const [scoreBoard, setScoreBoard] = useState([]);
   const { challengeData } = useChallenges();
+  const { user, setUser } = useAuthContext();
+
+  const getUsers = async () => {
+    fetch(`${import.meta.env.VITE_API_URL}/users`)
+      .then((response) => response.json())
+      .then((data) => setUsers(data))
+      .catch((err) => setErrors(err));
+  };
 
   if (challengeData) {
     console.log(challengeData);
   }
+
+  const getChallengeName = (challengeId) => {
+    const challenge = challengeData.find(
+      (challenge) => challenge._id === challengeId
+    );
+    return challenge.name;
+  };
 
   // fetch scoreBoard
   const fetchScoreBoard = async () => {
@@ -20,13 +36,12 @@ const ScoreBoard = () => {
 
   useEffect(() => {
     fetchScoreBoard();
+    getUsers();
   }, []);
+  
 
   if (scoreBoard.length > 0) {
-    console.log(scoreBoard);
-  }
-
-  if (scoreBoard.length > 0) return (
+    return (
       <div className={styles.tableContainer}>
         <table className={styles.scoreboardTable}>
           <thead className={styles.header}>
@@ -50,18 +65,22 @@ const ScoreBoard = () => {
                     index % 2 === 0 ? styles.trEven : styles.trOdd)
                 }
               >
-                <td className={styles.td}>{scoreBoard.userId}</td>
-                <td className={styles.td}>{scoreBoard.challengeId}</td>
-                <td className={styles.td}>{scoreBoard.score}</td>
+                <td className={styles.td}>{user.firstName}</td>
+                <td className={styles.td}>
+                  {getChallengeName(score.challengeId)}
+                </td>
+                <td className={styles.td}>{score.score}</td>
                 <td className={styles.td}>
                   {new Date(score.createdAt).toLocaleString()}
                 </td>
               </tr>
             ))}
+            {console.log(scoreBoard)}
           </tbody>
         </table>
       </div>
     );
+  }
 };
 
 export default ScoreBoard;
